1e7edd16d93cbfc752b9da230c05f339caad73be
Javier Lorenzo Carrion <javierlorenzocarrion@gmail.com>
2025-04-02 11:21:53 +0100

    refactor: extratc common objects spy instance types before each test and rename spy for stop and start recording

--- src/tests/surveillanceController.test.ts
+++ src/tests/surveillanceController.test.ts
@@ -1,4 +1,5 @@
 import {MotionSensor, SurveillanceController, VideoRecorder} from "../core/surveillanceController";
+import SpyInstance = jest.SpyInstance;
 
 class FakeSensor implements MotionSensor {
     isDetectingMotion(): boolean {
@@ -19,33 +20,31 @@ describe("Video Surveillance controller should", () => {
     let sensor: FakeSensor;
     let recorder: FakeRecorder;
     let controller: SurveillanceController;
+    let stubSensor: SpyInstance, spyRecorderOnStart: SpyInstance, spyRecorderOnStop: SpyInstance;
     beforeEach(() => {
         sensor = new FakeSensor();
         recorder = new FakeRecorder();
         controller = new SurveillanceController(sensor, recorder);
+        stubSensor = jest.spyOn(sensor, "isDetectingMotion");
+        spyRecorderOnStart = jest.spyOn(recorder, "startRecording");
+        spyRecorderOnStop = jest.spyOn(recorder, "stopRecording");
     })
     it("ask the recorder to stop recording when the sensor detects no motion", ()=>{
-        const stubSensor = jest.spyOn(sensor, "isDetectingMotion");
         stubSensor.mockImplementationOnce(() => false);
-        const spyRecorder = jest.spyOn(recorder, "stopRecording");
         controller.recordMotion();
-        expect(spyRecorder).toHaveBeenCalled();
+        expect(spyRecorderOnStop).toHaveBeenCalled();
     })
     it("ask the recorder to start recording when the sensor detects motion", ()=>{
-        const stubSensor = jest.spyOn(sensor, "isDetectingMotion");
         stubSensor.mockImplementationOnce(() => true);
-        const spyRecorder = jest.spyOn(recorder, "startRecording");
         controller.recordMotion();
-        expect(spyRecorder).toHaveBeenCalled();
+        expect(spyRecorderOnStart).toHaveBeenCalled();
     })
     it("ask the recorder to stop recording when the sensor throws an error", ()=>{
-        const stubSensor = jest.spyOn(sensor, "isDetectingMotion");
         stubSensor.mockImplementationOnce(() => {
             throw new Error("Error");
         });
-        const spyRecorder = jest.spyOn(recorder, "stopRecording");
         controller.recordMotion();
-        expect(spyRecorder).toHaveBeenCalled();
+        expect(spyRecorderOnStop).toHaveBeenCalled();
     })
 })
 
