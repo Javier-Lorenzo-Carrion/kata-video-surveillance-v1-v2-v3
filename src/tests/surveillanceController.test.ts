f425918b124edfa332c36ff1683a7a9102d13d94
Javier Lorenzo Carrion <javierlorenzocarrion@gmail.com>
2025-04-02 10:52:54 +0100

    feat: implement stub sensor with a defined behaviour for each test and extracts common instances

--- src/tests/surveillanceController.test.ts
+++ src/tests/surveillanceController.test.ts
@@ -25,7 +25,16 @@ describe("Video Surveillance controller should", () => {
         controller = new SurveillanceController(sensor, recorder);
     })
     it("ask the recorder to stop recording when the sensor detects no motion", ()=>{
+        const stubSensor = jest.spyOn(sensor, "isDetectingMotion");
+        stubSensor.mockImplementationOnce(() => false);
         const spyRecorder = jest.spyOn(recorder, "stopRecording");
+        controller.recordMotion();
+        expect(spyRecorder).toHaveBeenCalled();
+    })
+    it("ask the recorder to start recording when the sensor detects motion", ()=>{
+        const stubSensor = jest.spyOn(sensor, "isDetectingMotion");
+        stubSensor.mockImplementationOnce(() => true);
+        const spyRecorder = jest.spyOn(recorder, "startRecording");
         controller.recordMotion();
         expect(spyRecorder).toHaveBeenCalled();
     })
